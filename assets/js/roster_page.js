8671203639c5d019caa424a4b488c0307767ad02
donaldzou <donaldzou@live.hk>
2018-10-13 15:55:47 -0300

    update

---
+++ assets/js/roster_page.js
@@ -0,0 +1,15 @@
+function detectmob() { 
+    if( navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+    ){
+       window.location.href = "https://rns.myschoolapp.com/ftpimages/467/download/download_2620896.pdf";
+     }
+    else {
+       return false;
+     }
+   }
