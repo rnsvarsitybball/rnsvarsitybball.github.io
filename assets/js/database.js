3a76ad5fa1e9cabf92e67938aae46dace8f3f4cf
donaldzou <donaldzou@live.hk>
2018-10-13 15:33:05 -0300

    big changes

---
+++ assets/js/database.js
@@ -0,0 +1,69 @@
+var game = [
+    {
+        type:"footage",
+        game_date:"OCT 12, 2018",
+        game_team:"Halifax Prep",
+        game_location:"Halifax, NS",
+        game_pic:"images/GAME3.png",
+        game_link:"https://drive.google.com/drive/folders/1jtT-FCcgWJEPiJe-O_xRQhbbIP2TZqG1?usp=sharing"
+    },
+    {
+        type:"footage",
+        game_date:"SEP 28, 2018",
+        game_team:"Lee Academy",
+        game_location:"Lee Academy, MA",
+        game_pic:"images/GAME2.png",
+        game_link:"https://drive.google.com/drive/folders/1OU6CzHoxoOv_E1eyYx35TCZUaEQeu8pa?usp=sharing"
+    },
+    {
+        type:"footage",
+        game_date:"SEP 20, 2018",
+        game_team:"Prince Edward Island",
+        game_location:"Rothesay Netherwood School, NB",
+        game_pic:"images/GAME1.png",
+        game_link:"https://drive.google.com/drive/folders/1aEbDvJ6khVaJuQRAov8ATB1j5rHlYhnQ?usp=sharing"
+    }
+];
+var date = [
+    {
+        type:"schedule",
+        game_date:"OCTOBER 25-28, 2018",
+        game_team:"NPA SESSION #1",
+        game_location:"Rothesay Netherwood School, NB, CA"
+    }
+]
+
+var menu = [
+    {
+        type:"menu",title:"HOME",link:"/"
+    },
+    {
+        type:"menu",title:"ABOUT",link:"about.html"
+    },
+    {
+        type:"menu", title:"GAME FOTTAGE", link:"fottage_page.html"
+    },
+    {
+        type:"menu", title:"OUR TEAM", link:"roster.html"
+    }
+]
+
+
+
+
+
+var options = {
+    caseSensitive: true,
+    matchAllTokens: true,
+    threshold: 0.0,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    minMatchCharLength: 1,
+    keys: [
+        "type", 
+    ]
+};
+var fuse = new Fuse(game, options); // "list" is the item array
+var fuse1 = new Fuse(date, options);
+var fuse2 = new Fuse(menu, options);
---
+++ assets/js/fottage_page.js
@@ -0,0 +1,15 @@
+var footage_result = fuse.search("footage");
+for (var i = 0; i<footage_result.length; i++){
+    var current_html = $(".tiles").html();
+    var game_num = footage_result.length-i;
+    var color = Math.floor(Math.random()*6)+1;
+    var new_html = current_html + '<article class="style'+color+'"><span class="image"><img src="'+footage_result[i].game_pic+'" alt="" /></span><a href="'+footage_result[i].game_link+'"><h2>GAME #'+game_num+'</h2><h2>w/ '+footage_result[i].game_team+'</h2><h3>'+footage_result[i].game_date+'</h3><div class="content"><p>'+footage_result[i].game_location+'</p></div></a></article>'
+    $(".tiles").html(new_html);
+}
+
+var menu = fuse2.search("menu");
+for (var i = 0; i<menu.length; i++){
+    var current_html = $(".menu_list").html();
+    var new_html = current_html + '<li><a href="'+menu[i].link+'">'+menu[i].title+'</a></li>'
+    $(".menu_list").html(new_html);
+}
