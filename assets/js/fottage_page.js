55a9378c5d7df4dd27f9e1dd7183785655d89143
donaldzou <donaldzou@live.hk>
2018-12-03 23:53:52 -0400

    m

--- assets/js/fottage_page.js
+++ assets/js/fottage_page.js
@@ -3,7 +3,7 @@ for (var i = 0; i<game.length; i++){
     var current_html = $(".tiles").html();
     var game_num = game.length-i;
     var color = Math.floor(Math.random()*6)+1;
-    var new_html = current_html + '<article class="style'+color+' '+i+'game wow animated fadeInUp"><span class="image"><img src="'+game[i].game_pic+'" alt="" /></span><a href="'+game[i].game_link+'"><h2>GAME #'+game_num+'</h2><h2>w/ '+game[i].game_team+'</h2><h3>'+game[i].game_date+'</h3><div class="content"><h4 class="score">'+game[i].game_score+'</h4><p>'+game[i].game_location+'</p></div></a></article>'
+    var new_html = current_html + '<article class="style'+color+' '+i+'game animated fadeInUp"><span class="image"><img src="'+game[i].game_pic+'" alt="" /></span><a href="'+game[i].game_link+'"><h2>GAME #'+game_num+'</h2><h2>w/ '+game[i].game_team+'</h2><h3>'+game[i].game_date+'</h3><div class="content"><h4 class="score">'+game[i].game_score+'</h4><p>'+game[i].game_location+'</p></div></a></article>'
     var noscore = "."+i+"game .score";
     if($(noscore).html() == undefined){
         $(noscore).html("");
