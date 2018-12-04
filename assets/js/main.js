a58b2de79a2532697ca91e6db9df01321f2ac55d
donaldzou <donaldzou@live.hk>
2018-12-03 23:49:26 -0400

    m

--- assets/js/main.js
+++ assets/js/main.js
@@ -234,17 +234,7 @@ $(document).ready(function(){
 		$("body").addClass("animated fadeOutDown");
 		setTimeout(function(){
 			window.location = href;
-			if( navigator.userAgent.match(/Android/i)
-				|| navigator.userAgent.match(/webOS/i)
-				|| navigator.userAgent.match(/iPhone/i)
-				|| navigator.userAgent.match(/iPad/i)
-				|| navigator.userAgent.match(/iPod/i)
-				|| navigator.userAgent.match(/BlackBerry/i)
-				|| navigator.userAgent.match(/Windows Phone/i)
-				){
-					$("body").removeClass("fadeOutDown");
-					$("body").addClass("fadeIn slower");
-				 }
+			
 			
 		},500);
 	});
@@ -255,17 +245,7 @@ $(document).ready(function(){
 	$("body").addClass("animated fadeOutRight");
 	
 		setTimeout(function(){
-			if( navigator.userAgent.match(/Android/i)
-				|| navigator.userAgent.match(/webOS/i)
-				|| navigator.userAgent.match(/iPhone/i)
-				|| navigator.userAgent.match(/iPad/i)
-				|| navigator.userAgent.match(/iPod/i)
-				|| navigator.userAgent.match(/BlackBerry/i)
-				|| navigator.userAgent.match(/Windows Phone/i)
-				){
-					$("body").removeClass("fadeOutDown");
-					$("body").addClass("fadeIn slower");
-				 }
+			
 		 window.location = href;
 	},1700);
 	   });
