16231e1643345070556ee018dcfd7321178ffefd
donaldzou <donaldzou@live.hk>
2019-03-20 15:30:27 -0300

    as

--- assets/js/database.js
+++ assets/js/database.js
@@ -1,272 +1,272 @@
 var game = [
     {
-        type:"footage",
-        game_date:"JAN 26, 2019",
-        game_num: "SENIOR NIGHT",
-        game_team:"Blue Storm All-Stars",
-        game_location:"Rothesay Netherwood School, NB",
-        game_pic:"images/GAME6.png",
-        game_link:"https://www.youtube.com/watch?v=Ky3Ikww7oV0&feature=youtu.be",
-        game_score:"-"
+        "type" :"footage",
+        "game_date":"JAN 26, 2019",
+        "game_num": "SENIOR NIGHT",
+        "game_team":"Blue Storm All-Stars",
+        "game_location":"Rothesay Netherwood School, NB",
+        "game_pic":"images/GAME6.png",
+        "game_link":"https://www.youtube.com/watch?v=Ky3Ikww7oV0&feature=youtu.be",
+        "game_score":"-"
     },
     {
-        type:"footage",
-        game_date:"JAN 20, 2019",
-        game_num: 23,
-        game_team:"Thurgood Marshall Academy",
-        game_location:"Holy Cross High School, NY",
-        game_pic:"images/goth.jpg",
-        game_link:"https://www.youtube.com/watch?v=itFtTQulzic",
-        game_score:"-"
+        "type":"footage",
+        "game_date":"JAN 20, 2019",
+        "game_num": 23,
+        "game_team":"Thurgood Marshall Academy",
+        "game_location":"Holy Cross High School, NY",
+        "game_pic":"images/goth.jpg",
+        "game_link":"https://www.youtube.com/watch?v=itFtTQulzic",
+        "game_score":"-"
     },
     {
-        type:"footage",
-        game_date:"JAN 20, 2019",
-        game_num: 22,
-        game_team:"Eduprize School",
-        game_location:"Holy Cross High School, NY",
-        game_pic:"images/goth.jpg",
-        game_link:"https://www.youtube.com/watch?v=U_AZs1V_aAU",
-        game_score:"-"
+        "type":"footage",
+        "game_date":"JAN 20, 2019",
+        "game_num": 22,
+        "game_team":"Eduprize School",
+        "game_location":"Holy Cross High School, NY",
+        "game_pic":"images/goth.jpg",
+        "game_link":"https://www.youtube.com/watch?v=U_AZs1V_aAU",
+        "game_score":"-"
     },
     {
-        type:"footage",
-        game_date:"JAN 19, 2019",
-        game_num: 21,
-        game_team:"Eduprize School",
-        game_location:"Holy Cross High School, NY",
-        game_pic:"images/goth.jpg",
-        game_link:"https://www.youtube.com/watch?v=NXbDqlcH1tw",
-        game_score:"-"
+        "type":"footage",
+        "game_date":"JAN 19, 2019",
+        "game_num": 21,
+        "game_team":"Eduprize School",
+        "game_location":"Holy Cross High School, NY",
+        "game_pic":"images/goth.jpg",
+        "game_link":"https://www.youtube.com/watch?v=NXbDqlcH1tw",
+        "game_score":"-"
     },
     {
-        type:"footage",
-        game_date:"JAN 19, 2019",
-        game_num: 20,
-        game_team:"Broome Street Academy",
-        game_location:"Holy Cross High School, NY",
-        game_pic:"images/goth.jpg",
-        game_link:"https://www.youtube.com/watch?v=EgqS5WQJ9CE",
-        game_score:"-"
+        "type":"footage",
+        "game_date":"JAN 19, 2019",
+        "game_num": 20,
+        "game_team":"Broome Street Academy",
+        "game_location":"Holy Cross High School, NY",
+        "game_pic":"images/goth.jpg",
+        "game_link":"https://www.youtube.com/watch?v=EgqS5WQJ9CE",
+        "game_score":"-"
     },
     {
-        type:"footage",
-        game_date:"DEC 16, 2018",
-        game_num: 19,
-        game_team:"BMHS",
-        game_location:"Fredericton High School, NB",
-        game_pic:"images/GAME8.jpg",
-        game_link:"https://youtu.be/oViDZK_75Fs",
-        game_score:"WIN/ 105 - 66"
+        "type":"footage",
+        "game_date":"DEC 16, 2018",
+        "game_num": 19,
+        "game_team":"BMHS",
+        "game_location":"Fredericton High School, NB",
+        "game_pic":"images/GAME8.jpg",
+        "game_link":"https://youtu.be/oViDZK_75Fs",
+        "game_score":"WIN/ 105 - 66"
     },
     {
-        type:"footage",
-        game_date:"DEC 14, 2018",
-        game_num: 18,
-        game_team:"Leo Haeys",
-        game_location:"Bliss Carmen Middle School, NB",
-        game_pic:"images/GAME7.jpg",
-        game_link:"https://youtu.be/nC1pCrjglDs",
-        game_score:"WIN/ 102 - 56"
+        "type":"footage",
+        "game_date":"DEC 14, 2018",
+        "game_num": 18,
+        "game_team":"Leo Haeys",
+        "game_location":"Bliss Carmen Middle School, NB",
+        "game_pic":"images/GAME7.jpg",
+        "game_link":"https://youtu.be/nC1pCrjglDs",
+        "game_score":"WIN/ 102 - 56"
     },
     {
-        type:"footage",
-        game_date:"DEC 8, 2018",
-        game_num: 17,
-        game_team:"Halifax Prep",
-        game_location:"Rothesay Netherwood School, NB",
-        game_pic:"images/GAME5.png",
-        game_link:"https://youtu.be/yTfm318QmPs",
-        game_score:"WIN/ 95 - 93"
+        "type":"footage",
+        "game_date":"DEC 8, 2018",
+        "game_num": 17,
+        "game_team":"Halifax Prep",
+        "game_location":"Rothesay Netherwood School, NB",
+        "game_pic":"images/GAME5.png",
+        "game_link":"https://youtu.be/yTfm318QmPs",
+        "game_score":"WIN/ 95 - 93"
     },
     {
-        type:"footage",
-        game_date:"NOV 30, 2018",
-        game_num: 16,
-        game_team:"Oakville Basketball Prep",
-        game_location:"Notre Dame High School, ON",
-        game_pic:"images/GAME5.png",
-        game_link:"https://youtu.be/fgnHEpMxrWM",
-        game_score:"WIN/ 96 - 74"
+        "type":"footage",
+        "game_date":"NOV 30, 2018",
+        "game_num": 16,
+        "game_team":"Oakville Basketball Prep",
+        "game_location":"Notre Dame High School, ON",
+        "game_pic":"images/GAME5.png",
+        "game_link":"https://youtu.be/fgnHEpMxrWM",
+        "game_score":"WIN/ 96 - 74"
     },
     {
-        type:"footage",
-        game_date:"NOV 30, 2018",
-        game_num: 15,
-        game_team:"EDGE SCHOOL",
-        game_location:"Notre Dame High School, ON",
-        game_pic:"images/GAME5.png",
-        game_link:"https://youtu.be/j9Byr-cwJH4",
-        game_score:"WIN/ 96 - 85"
+        "type":"footage",
+        "game_date":"NOV 30, 2018",
+        "game_num": 15,
+        "game_team":"EDGE SCHOOL",
+        "game_location":"Notre Dame High School, ON",
+        "game_pic":"images/GAME5.png",
+        "game_link":"https://youtu.be/j9Byr-cwJH4",
+        "game_score":"WIN/ 96 - 85"
     },
     {
-        type:"footage",
-        game_date:"NOV 29, 2018",
-        game_num: 14,
-        game_team:"B.E.A.S.T PREP",
-        game_location:"Notre Dame High School, ON",
-        game_pic:"images/GAME5.png",
-        game_link:"https://youtu.be/j9Byr-cwJH4",
-        game_score:"WIN/ 95 - 71"
+        "type":"footage",
+        "game_date":"NOV 29, 2018",
+        "game_num": 14,
+        "game_team":"B.E.A.S.T PREP",
+        "game_location":"Notre Dame High School, ON",
+        "game_pic":"images/GAME5.png",
+        "game_link":"https://youtu.be/j9Byr-cwJH4",
+        "game_score":"WIN/ 95 - 71"
     },
     {
-        type:"footage",
-        game_date:"NOV 28, 2018",
-        game_num: 13,
-        game_team:"LONDON BASKETBALL ACADEMY",
-        game_location:"Notre Dame High School, ON",
-        game_pic:"images/GAME5.png",
-        game_link:"https://youtu.be/Z30-fFhW1HQ",
-        game_score:"WIN/ 87 - 57"
+        "type":"footage",
+        "game_date":"NOV 28, 2018",
+        "game_num": 13,
+        "game_team":"LONDON BASKETBALL ACADEMY",
+        "game_location":"Notre Dame High School, ON",
+        "game_pic":"images/GAME5.png",
+        "game_link":"https://youtu.be/Z30-fFhW1HQ",
+        "game_score":"WIN/ 87 - 57"
     },
     {
-        type:"footage",
-        game_date:"NOV 25, 2018",
-        game_num: 12,
-        game_team:"Bernice MacNaughton High School",
-        game_location:"Rothesay Netherwood School, NB",
-        game_pic:"images/GAME6.png",
-        game_link:"https://youtu.be/beUIGJgv_r4"
+        "type":"footage",
+        "game_date":"NOV 25, 2018",
+        "game_num": 12,
+        "game_team":"Bernice MacNaughton High School",
+        "game_location":"Rothesay Netherwood School, NB",
+        "game_pic":"images/GAME6.png",
+        "game_link":"https://youtu.be/beUIGJgv_r4"
     },
     {
-        type:"footage",
-        game_date:"NOV 24, 2018",
-        game_num: 11,
-        game_team:"Charles P. Allen High School",
-        game_location:"Rothesay Netherwood School, NB",
-        game_pic:"images/GAME6.png",
-        game_link:"https://youtu.be/CHDNe9PPBW8"
+        "type":"footage",
+        "game_date":"NOV 24, 2018",
+        "game_num": 11,
+        "game_team":"Charles P. Allen High School",
+        "game_location":"Rothesay Netherwood School, NB",
+        "game_pic":"images/GAME6.png",
+        "game_link":"https://youtu.be/CHDNe9PPBW8"
     },
     {
-        type:"footage",
-        game_date:"NOV 24, 2018",
-        game_num: 10,
-        game_team:"Kennebecasis Valley High School",
-        game_location:"Rothesay Netherwood School, NB",
-        game_pic:"images/GAME6.png",
-        game_link:"https://youtu.be/aL0NGIT0J8o"
+        "type":"footage",
+        "game_date":"NOV 24, 2018",
+        "game_num": 10,
+        "game_team":"Kennebecasis Valley High School",
+        "game_location":"Rothesay Netherwood School, NB",
+        "game_pic":"images/GAME6.png",
+        "game_link":"https://youtu.be/aL0NGIT0J8o"
     },
     {
-        type:"footage",
-        game_date:"NOV 23, 2018",
-        game_num: 9,
-        game_team:"Fredericton High School",
-        game_location:"Rothesay Netherwood School, NB",
-        game_pic:"images/GAME6.png",
-        game_link:"https://youtu.be/XvY_F6yEGy0"
+        "type":"footage",
+        "game_date":"NOV 23, 2018",
+        "game_num": 9,
+        "game_team":"Fredericton High School",
+        "game_location":"Rothesay Netherwood School, NB",
+        "game_pic":"images/GAME6.png",
+        "game_link":"https://youtu.be/XvY_F6yEGy0"
     },
     {
-        type:"footage",
-        game_date:"OCT 28, 2018",
-        game_num: 8,
-        game_team:"Toronto Basketball Academy",
-        game_location:"Rothesay Netherwood School, NB",
-        game_pic:"images/GAME5.png",
-        game_link:"https://youtu.be/GVdhY7rac7Y"
+        "type":"footage",
+        "game_date":"OCT 28, 2018",
+        "game_num": 8,
+        "game_team":"Toronto Basketball Academy",
+        "game_location":"Rothesay Netherwood School, NB",
+        "game_pic":"images/GAME5.png",
+        "game_link":"https://youtu.be/GVdhY7rac7Y"
     },
     {
-        type:"footage",
-        game_date:"OCT 27, 2018",
-        game_num: 7,
-        game_team:"GTA Prep",
-        game_location:"Rothesay Netherwood School, NB",
-        game_pic:"images/GAME5.png",
-        game_link:"https://youtu.be/WaluBDbq3mE"
+        "type":"footage",
+        "game_date":"OCT 27, 2018",
+        "game_num": 7,
+        "game_team":"GTA Prep",
+        "game_location":"Rothesay Netherwood School, NB",
+        "game_pic":"images/GAME5.png",
+        "game_link":"https://youtu.be/WaluBDbq3mE"
     },
     {
-        type:"footage",
-        game_date:"OCT 26, 2018",
-        game_num: 6,
-        game_team:"Crestwood",
-        game_location:"Rothesay Netherwood School, NB",
-        game_pic:"images/GAME5.png",
-        game_link:"https://youtu.be/zIyELDnHuE0"
+        "type":"footage",
+        "game_date":"OCT 26, 2018",
+        "game_num": 6,
+        "game_team":"Crestwood",
+        "game_location":"Rothesay Netherwood School, NB",
+        "game_pic":"images/GAME5.png",
+        "game_link":"https://youtu.be/zIyELDnHuE0"
     },
     {
-        type:"footage",
-        game_date:"OCT 25, 2018",
-        game_num: 5,
-        game_team:"B.E.A.S.T. Prep",
-        game_location:"Rothesay Netherwood School, NB",
-        game_pic:"images/GAME5.png",
-        game_link:"https://youtu.be/WCkwH_WrhuA"
+        "type":"footage",
+        "game_date":"OCT 25, 2018",
+        "game_num": 5,
+        "game_team":"B.E.A.S.T. Prep",
+        "game_location":"Rothesay Netherwood School, NB",
+        "game_pic":"images/GAME5.png",
+        "game_link":"https://youtu.be/WCkwH_WrhuA"
     },
     {
-        type:"footage",
-        game_date:"OCT 16, 2018",
-        game_num: 4,
-        game_team:"Friendly",
-        game_location:"Rothesay Netherwood School, NB",
-        game_pic:"images/GAME4.png",
-        game_link:"https://drive.google.com/drive/folders/1H2h10Y72C9UIOv7EBYAJ5nQeQbVieAqd?usp=sharing"
+        "type":"footage",
+        "game_date":"OCT 16, 2018",
+        "game_num": 4,
+        "game_team":"Friendly",
+        "game_location":"Rothesay Netherwood School, NB",
+        "game_pic":"images/GAME4.png",
+        "game_link":"https://drive.google.com/drive/folders/1H2h10Y72C9UIOv7EBYAJ5nQeQbVieAqd?usp=sharing"
     },
     {
-        type:"footage",
-        game_date:"OCT 12, 2018",
-        game_num: 3,
-        game_team:"Halifax Prep",
-        game_location:"Halifax, NS",
-        game_pic:"images/GAME3.png",
-        game_link:"https://drive.google.com/drive/folders/1jtT-FCcgWJEPiJe-O_xRQhbbIP2TZqG1?usp=sharing"
+        "type":"footage",
+        "game_date":"OCT 12, 2018",
+        "game_num": 3,
+        "game_team":"Halifax Prep",
+        "game_location":"Halifax, NS",
+        "game_pic":"images/GAME3.png",
+        "game_link":"https://drive.google.com/drive/folders/1jtT-FCcgWJEPiJe-O_xRQhbbIP2TZqG1?usp=sharing"
     },
     {
-        type:"footage",
-        game_num: 2,
-        game_date:"SEP 28, 2018",
-        game_team:"Lee Academy",
-        game_location:"Lee Academy, MA",
-        game_pic:"images/GAME2.png",
-        game_link:"https://drive.google.com/drive/folders/1OU6CzHoxoOv_E1eyYx35TCZUaEQeu8pa?usp=sharing"
+        "type":"footage",
+        "game_num": 2,
+        "game_date":"SEP 28, 2018",
+        "game_team":"Lee Academy",
+        "game_location":"Lee Academy, MA",
+        "game_pic":"images/GAME2.png",
+        "game_link":"https://drive.google.com/drive/folders/1OU6CzHoxoOv_E1eyYx35TCZUaEQeu8pa?usp=sharing"
     },
     {
-        type:"footage",
-        game_num: 1,
-        game_date:"SEP 20, 2018",
-        game_team:"Prince Edward Island",
-        game_location:"Rothesay Netherwood School, NB",
-        game_pic:"images/GAME1.png",
-        game_link:"https://drive.google.com/drive/folders/1aEbDvJ6khVaJuQRAov8ATB1j5rHlYhnQ?usp=sharing"
+        "type":"footage",
+        "game_num": 1,
+        "game_date":"SEP 20, 2018",
+        "game_team":"Prince Edward Island",
+        "game_location":"Rothesay Netherwood School, NB",
+        "game_pic":"images/GAME1.png",
+        "game_link":"https://drive.google.com/drive/folders/1aEbDvJ6khVaJuQRAov8ATB1j5rHlYhnQ?usp=sharing"
     }
 ];
 var date = [
     // {
-    //     type:"schedule",
-    //     game_date:"JAN 19, 2019 - 11:00 AM",
-    //     game_team:"BROOME STREET ACADEMY",
-    //     game_location:"Holy Cross High School, NY"
+    //     "type":"schedule",
+    //     "game_date":"JAN 19, 2019 - 11:00 AM",
+    //     "game_team":"BROOME STREET ACADEMY",
+    //     "game_location":"Holy Cross High School, NY"
     // },
     // {
-    //     type:"schedule",
-    //     game_date:"JAN 19, 2019 - 17:00 PM",
-    //     game_team:"EDUPRIZE SCHOOL",
-    //     game_location:"Holy Cross High School, NY"
+    //     "type":"schedule",
+    //     "game_date":"JAN 19, 2019 - 17:00 PM",
+    //     "game_team":"EDUPRIZE SCHOOL",
+    //     "game_location":"Holy Cross High School, NY"
     // },
     // {
-    //     type:"schedule",
-    //     game_date:"JAN 20, 2019 - 15:30 PM",
-    //     game_team:"THURGOOD MARSHALL ACADEMY",
-    //     game_location:"Holy Cross High School, NY"
+    //     "type":"schedule",
+    //     "game_date":"JAN 20, 2019 - 15:30 PM",
+    //     "game_team":"THURGOOD MARSHALL ACADEMY",
+    //     "game_location":"Holy Cross High School, NY"
     // }
 ]
 
 var menu = [
     {
-        type:"menu",title:"HOME",link:"/"
+        "type":"menu",title:"HOME",link:"/"
     },
     {
-        type:"menu", title:"GALLERY", link:"gallery.html"
+        "type":"menu", title:"GALLERY", link:"gallery.html"
     },
     {
-        type:"menu",title:"ABOUT",link:"about.html"
+        "type":"menu",title:"ABOUT",link:"about.html"
     },
     {
-        type:"menu", title:"GAME FOOTAGE", link:"fottage_page.html"
+        "type":"menu", title:"GAME FOOTAGE", link:"fottage_page.html"
     },
     {
-        type:"menu", title:"OUR TEAM", link:"roster.html"
+        "type":"menu", title:"OUR TEAM", link:"roster.html"
     },
     {
-        type:"menu", title:"LIVE STREAM", link:"live.html"
+        "type":"menu", title:"LIVE STREAM", link:"live.html"
     }
     
 ]
